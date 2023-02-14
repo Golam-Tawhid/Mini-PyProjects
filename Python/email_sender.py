67991456a1f89b8899508f2d58226781ffb7bff2
Golam Tawhid <tawhidfahad199@gmail.com>
2023-02-14 23:39:23 +0600

    no changes

---
+++ Python/email_sender.py
@@ -0,0 +1,22 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+e_send='golamthauhid@gmail.com'
+e_pass='kbnwyozqemxgbsfr'
+
+e_receive='yarejo6149@mirtox.com'
+sub='Python project check'
+body="""
+I made an python project that can send emails.
+"""
+em= EmailMessage()
+em['From']=e_send
+em['To']=e_receive
+em['Subject']=sub
+em.set_content(body)
+
+context= ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+    smtp.login(e_send, e_pass)
+    smtp.sendmail(e_send, e_receive, em.as_string())
